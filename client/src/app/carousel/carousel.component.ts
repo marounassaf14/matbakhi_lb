07731b4cd1b711e2f7c9f895bd07df41217df2b1
camildaou <camil.daou@hotmail.com>
2024-12-05 00:29:07 +0200

    fixed burger+carousel+misc...

---
+++ client/src/app/carousel/carousel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
+@Component({
+  selector: 'app-carousel',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './carousel.component.html',
+  styleUrl: './carousel.component.css'
+})
+export class CarouselComponent {
+
+}
